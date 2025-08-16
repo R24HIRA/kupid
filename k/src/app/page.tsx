1f4987aac43c1befb2ca3a53c4e4d25c9cc89fc9
Raj Hira <rajhira24@gmail.com>
2025-08-16 15:27:25 -0400

    handleClearCompleted

--- k/src/app/page.tsx
+++ k/src/app/page.tsx
@@ -55,8 +55,12 @@ export default function Home() {
    const handleDeleteTodo = (id: string) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-
-
+  
+  //Clear
+  const handleClearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+  
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
       <div className="max-w-4xl mx-auto">
